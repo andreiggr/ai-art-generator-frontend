488c152dad61a0dccc8dbdf62230b4741e33b09a
andreiggr <andrei.ggr@gmail.com>
2023-02-18 22:01:00 +0200

    heroku api

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-//const apiPath = "https://print-ai-art-backend.herokuapp.com";
+const apiPath = "https://print-ai-art-backend.herokuapp.com";
 
-const apiPath = "http://localhost:8080";
+//const apiPath = "http://localhost:8080";
 
 export { apiPath };
