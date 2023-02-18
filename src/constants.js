a1c520904d5eacdefbce0f22e02adf13a0183e9c
andreiggr <andrei.ggr@gmail.com>
2023-02-18 21:58:55 +0200

    Changes in api. Listing from lexica

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
-const apiPath = "https://print-ai-art-backend.herokuapp.com";
+//const apiPath = "https://print-ai-art-backend.herokuapp.com";
+
+const apiPath = "http://localhost:8080";
 
 export { apiPath };
